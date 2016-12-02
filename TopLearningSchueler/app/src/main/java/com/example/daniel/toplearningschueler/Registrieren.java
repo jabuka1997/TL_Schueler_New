2ff7647ba161055e3c7281891f4c6dbe3fb89437
jabuka1997 <Administrator@NB8FK38.htl-donaustadt.at>
2016-12-02 09:31:52 +0100

    register

--- TopLearningSchueler/app/src/main/java/com/example/daniel/toplearningschueler/Registrieren.java
+++ TopLearningSchueler/app/src/main/java/com/example/daniel/toplearningschueler/Registrieren.java
@@ -1,18 +1,28 @@
 package com.example.daniel.toplearningschueler;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.support.v4.os.EnvironmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Registrieren extends Fragment {
+public class Registrieren extends Fragment implements View.OnClickListener {
 
+    Button btn_reg;
 
     public Registrieren() {
         // Required empty public constructor
@@ -24,10 +34,19 @@ public class Registrieren extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_registrieren, container, false);
-
         getActivity().setTitle("Registrieren");
-        // Inflate the layout for this fragment
+
+        btn_reg = (Button) view.findViewById(R.id.btn_register);
+        btn_reg.setOnClickListener(this);
+
+
         return view;
     }
 
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
